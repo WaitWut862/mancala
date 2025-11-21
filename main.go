30ac18988e8a17334071990614897671f675e43a
Carson Morton <cgmorton862@gmail.com>
2025-11-21 16:07:27 -0500

    added rendering

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Config struct {
 
 type Game struct {
 	Board []int
-	Config Config
+	Config *Config
 	Turn int
 }
 
@@ -20,12 +20,13 @@ type Game struct {
 func main() {
 	g := &Game{}
 	g.initGame()
-	fmt.Println(g.Board)
+	g.render()
 }
 
 
 func (g *Game) initGame() {
-	c := &Config{}
+	g.Config = &Config{}
+	c := g.Config
 	fmt.Println("enter desired board size (default 6) ")
 	fmt.Scanln(&c.CellsPerSide)
 	totalCells := (c.CellsPerSide * 2) + 2
@@ -51,3 +52,32 @@ func oppIndex(n int) int {
 	o := len(g.Board) -2 - n
 	return o
 }
+
+
+func (g *Game) render() {
+	side := g.Config.CellsPerSide
+	p1Store := side
+	p2Store := (side * 2) +1
+
+	fmt.Print("⌈  ⌉")
+	for i := (side * 2); i > side; i-- {
+		fmt.Printf("[%2d]", g.Board[i])
+	}
+	fmt.Print("⌈  ⌉\n")
+
+	fmt.Printf(" %2d ", g.Board[p2Store])
+
+	for i := 0; i < side; i++ {
+		fmt.Print("----")
+	}
+
+	fmt.Printf(" %2d \n", g.Board[p1Store])
+
+	fmt.Print("⌊  ⌋")
+	for i := 0; i < side; i++ {
+		fmt.Printf("[%2d]", g.Board[i])
+	}
+	fmt.Print("⌊  ⌋\n")
+}
+
+
